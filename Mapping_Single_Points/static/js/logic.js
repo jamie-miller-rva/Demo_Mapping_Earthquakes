aeab2f2562f85c89f7e062eb1cdd804139dfef9a
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2022-01-19 12:34:56 -0500

    mapping single points, circle

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,16 +8,16 @@ console.log("working");
 const API_KEY = MapboxApiKey;
 
 // Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Alternate method (pick one)
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
 
 // We create the tile layer that will be the background of our map.
 // see https://docs.mapbox.com/api/maps/styles/#mapbox-styles
@@ -42,3 +42,18 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
 
 // Then we add our 'streets' tile layer to the map.
 dark.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// see https://leafletjs.com/reference-1.6.0.html#marker
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// add a circle
+// see https://leafletjs.com/reference-1.6.0.html#circle
+// also see https://leafletjs.com/reference-1.6.0.html#circlemarker
+// see https://www.w3schools.com/cssref/css_colors.asp for colors examples
+L.circle([34.0522, -118.2437], {
+  color: 'black',
+  fillColor: 'LightYellow',
+  fillOpacity: 0.7,
+  radius: 300
+}).addTo(map);
