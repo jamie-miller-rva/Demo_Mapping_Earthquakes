9dba4386e69c8d49c44355df1145e187766dbe46
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2022-01-19 13:18:03 -0500

    adding circle for each city in cities

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,74 @@
+// my logic.js file
+// see https://docs.mapbox.com/help/glossary/ for mapbox help
+
+// confirm link is working
+console.log("working");
+
+// get API_KEY
+const API_KEY = MapboxApiKey;
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// Alternate method (pick one)
+// Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+// We create the tile layer that will be the background of our map.
+// see https://docs.mapbox.com/api/maps/styles/#mapbox-styles
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// add dark
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// Then we add our 'streets' tile layer to the map.
+dark.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// see https://leafletjs.com/reference-1.6.0.html#marker
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// add a circle
+// see https://leafletjs.com/reference-1.6.0.html#circle
+// also see https://leafletjs.com/reference-1.6.0.html#circlemarker
+// see https://www.w3schools.com/cssref/css_colors.asp for colors examples
+// L.circle([34.0522, -118.2437], {
+//   color: 'black',
+//   fillColor: 'LightYellow',
+//   fillOpacity: 0.7,
+//   radius: 300
+// }).addTo(map);
+
+// add marker for each city in citie
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+  console.log(city);
+  L.circleMarker(city.location, {
+    color: 'orange',
+    fillColor: 'orange',
+    weight: 4,
+    fillOpacity: 0.3,
+    radius: (city.population - 200000) /100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+ });
